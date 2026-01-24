1bfa85db50519069303c7bb58503b1646eda98ce
YogaDharma21 <yogacode2006@gmail.com>
2026-01-24 20:48:44 +0800

    feat: Add database maintenance functionalities for books, loans, and visitors, and implement a login user interface.

--- src/MaintainDataPackage/MaintainDatabase.java
+++ src/MaintainDataPackage/MaintainDatabase.java
@@ -289,10 +289,8 @@ public class MaintainDatabase {
             pre.setString(2, password);
             ResultSet resultset = pre.executeQuery();
             if (resultset.next()) {
-                System.out.println("Login Berhasil untuk user: " + username);
                 return true;
             } else {
-                System.out.println("Login Gagal: Username atau Password salah");
                 return false;
             }
         } catch (Exception e) {
