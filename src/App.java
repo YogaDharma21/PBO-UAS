d0823a338c8e431d66c67d4bf3b21fd3a9a58c51
komangyxy-commits <ikomangriskytriyanaputra@gmail.com>
2026-01-24 18:37:54 +0800

    revisiApp

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
 import ProjectInterface.LoginFrame;
 
-import java.sql.Connection;
-
-import DatabaseProcess.Koneksi;
 public class App {
     public static void main(String[] args) throws Exception {
-      Connection conn=Koneksi.getKoneksi();
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
     }
 }
+  
